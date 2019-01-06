dad45424e4c569076a7a6754da63b04156b26cd9
rohitmehlawat <rohitmehlawat27@gmail.com>
2019-01-06 16:55:00 +0530

    getEmployee API introduced

--- src/main/java/com/admin/controller/EmployeeController.java
+++ src/main/java/com/admin/controller/EmployeeController.java
@@ -1,15 +1,25 @@
 package com.admin.controller;
 
+import static com.admin.security.SecurityConstants.HEADER_STRING;
+import static com.admin.security.SecurityConstants.SECRET;
+import static com.admin.security.SecurityConstants.TOKEN_PREFIX;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.admin.domain.Employee;
+import com.admin.domain.UserDetail;
 import com.admin.repository.EmployeeRepository;
 import com.admin.util.ResponseUtil;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 
 @RestController
 @RequestMapping("/employee")
@@ -37,6 +47,23 @@ public class EmployeeController {
 		
 		return response;
 		
+	}
+	
+	@GetMapping("/getEmployee")
+	public ResponseUtil<Employee> getUser(HttpServletRequest request){
+		ResponseUtil<Employee> response=new ResponseUtil<>();
+		String token = request.getHeader(HEADER_STRING);
+		String user = (String) request.getAttribute("user");
+		if(!user.contains("@")) {
+			  response.setResponseObject(employeeRepository.findByPhoneNo(user));
+		      response.setStatus("success");
+		}
+		else {
+			response.setResponseObject(null);
+		      response.setStatus("failure");
+		}
+      
+		return response;
 	}
 	
 	
